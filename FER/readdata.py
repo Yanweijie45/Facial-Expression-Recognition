e4e92ee6897190f020530c9bb0d2d72720b4ad45
zheshishayouxi <57176049+zheshishayouxi@users.noreply.github.com>
2020-06-01 19:10:58 +0800

    Update readdata.py

--- FER/readdata.py
+++ FER/readdata.py
@@ -31,8 +31,3 @@ def __parse_function_csv(serial_exmp_):
     image_ = tf.image.resize_images(image_, [default_height, default_width])
     return image_, label_
 
-#获取数据集
-def get_dataset(record_name_):
-    record_path_ = os.path.join(data_folder_name, data_path_name, record_name_)
-    data_set_ = tf.data.TFRecordDataset(record_path_)
-    return data_set_.map(__parse_function_csv)
